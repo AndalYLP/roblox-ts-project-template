bba7b5eb17455b562f8ee65c8090c9bf13bd2f3d
AndalYLP <andal089.az@gmail.com>
2025-01-04 22:19:48 -0500

    feat: added withPlayerEntity for network events

---
+++ src/server/services/player/with-player-entity.ts
@@ -0,0 +1,43 @@
+import { Dependency } from "@flamework/core";
+import Log from "@rbxts/log";
+import { ServerResponse } from "types/interfaces/network";
+import { PlayerService } from ".";
+import { PlayerEntity } from "./entity";
+
+let playerService: PlayerService | undefined;
+
+/**
+ * This method wraps a callback and replaces the first argument (that must be of
+ * type `Player`) with that players `PlayerEntity` class. This is to be used
+ * when responding to network events. If wrapping server-side events, you should
+ * use `withPlayerEntity` from the `PlayerService` class.
+ * @template T - The type of the arguments passed to the callback.
+ * @param func - The callback to wrap.
+ * @returns The server response.
+ */
+export default function withPlayerEntity<T extends Array<unknown>>(
+	func: (playerEntity: PlayerEntity, ...args: T) => undefined | void
+): (player: Player, ...args: T) => ServerResponse {
+	if (!playerService) {
+		playerService = Dependency<PlayerService>();
+	}
+
+	return (player: Player, ...args: T) => {
+		const entity = playerService!.getPlayerEntity(player);
+		if (entity) {
+			return identity<ServerResponse>({
+				data: func(entity, ...args),
+				success: true
+			});
+		}
+
+		Log.Error(
+			`Unable to find entity for player ${player}, unable to call callback. Stacktrace: \n` + debug.traceback()
+		);
+
+		return identity<ServerResponse>({
+			error: "Internal error",
+			success: false
+		});
+	};
+}
