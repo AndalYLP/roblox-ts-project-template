08fc2c92f3190c572562123d37351abea8d0b768
AndalYLP <andal089.az@gmail.com>
2025-01-06 11:06:00 -0500

    feat: new debugging decorator

--- src/server/decorators/debugging/test-method.ts
+++ src/server/decorators/debugging/test-method.ts
@@ -1,11 +1,11 @@
 /* eslint-disable @typescript-eslint/naming-convention */
 
 import Log from "@rbxts/log";
-import { HttpService } from "@rbxts/services";
+import { TableToString } from "@rbxts/rbx-debug";
 
 export const TestMethod = <T extends object, P extends unknown[]>(...args: P) => {
 	return (target: T, propertyKey: string, descriptor: TypedPropertyDescriptor<(this: T, ...args: P) => void>) => {
-		Log.Debug(`running ${target}.${propertyKey} with arguments: {args}`, HttpService.JSONEncode(args));
+		Log.Debug(`running ${target}.${propertyKey} with arguments: {args}`, TableToString(args));
 		const returnValue = descriptor.value(target, ...args);
 		Log.Debug(`${target}.${propertyKey} returned: ${returnValue}`);
 	};
