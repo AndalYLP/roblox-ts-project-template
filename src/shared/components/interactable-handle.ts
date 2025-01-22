be67078190edeadb70d9da2d4e31633e7768919c
AndalYLP <andal089.az@gmail.com>
2025-01-22 13:11:32 -0500

    docs: added JSdoc

--- src/shared/components/interactable-handle.ts
+++ src/shared/components/interactable-handle.ts
@@ -7,6 +7,12 @@ export type InteractableInstances = ClickDetector | ProximityPrompt;
 
 export interface OnTrigger {
 	instance: InteractableInstances;
+	/**
+	 * Called when the player triggers the interactable instance, Triggered for
+	 * `ProximityPrompt` and LeftClick for `ClickDetector`.
+	 *
+	 * @param player - The player who triggered the interactable instance.
+	 */
 	onTrigger(player: Player): void;
 }
 
@@ -30,6 +36,12 @@ export class Interactable implements OnStart {
 		);
 	}
 
+	/**
+	 * Handles the addition of an interactable life cycle.
+	 *
+	 * @param event - The event data that includes the interactable instance and
+	 *   its onTrigger callback.
+	 */
 	private onAdded(event: OnTrigger): void {
 		const { instance } = event;
 
@@ -47,6 +59,12 @@ export class Interactable implements OnStart {
 		this.connections.set(instance, connection);
 	}
 
+	/**
+	 * Handles the removal of an interactable life cycle.
+	 *
+	 * @param event - The event data that includes the interactable instance to
+	 *   be removed.
+	 */
 	private onRemoved({ instance }: OnTrigger): void {
 		this.connections.get(instance)?.Disconnect();
 	}
