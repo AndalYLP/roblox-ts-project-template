79cb7f2c0ae730a5a30e69601cbbc9391ea49b06
AndalYLP <andal089.az@gmail.com>
2025-02-08 13:45:24 -0500

    feat: updated background-blur so you can disable the core gui

--- src/client/ui/components/background-blur.tsx
+++ src/client/ui/components/background-blur.tsx
@@ -1,12 +1,15 @@
 import { useBindingListener, useCamera } from "@rbxts/pretty-react-hooks";
-import React, { useState } from "@rbxts/react";
+import React, { useEffect, useState } from "@rbxts/react";
 import { createPortal } from "@rbxts/react-roblox";
+import { GuiService } from "@rbxts/services";
 
 import type { BindingValue } from "types/utils/react";
+import coreCall from "utils/core-call";
 
 interface BackgroundBlurProps {
 	/** The size of the blur effect. */
 	BlurSize?: BindingValue<number>;
+	DisableCoreGui?: boolean;
 }
 
 /**
@@ -16,13 +19,25 @@ interface BackgroundBlurProps {
  * @param props.BlurSize - The size of the blur effect.
  * @returns The rendered background blur component.
  */
-export function BackgroundBlur({ BlurSize }: Readonly<BackgroundBlurProps>): React.ReactNode {
+export function BackgroundBlur({
+	BlurSize,
+	DisableCoreGui = false,
+}: Readonly<BackgroundBlurProps>): React.ReactNode {
 	const camera = useCamera();
 	const [visible, setVisible] = useState(false);
 
 	useBindingListener(BlurSize, (size = 0) => {
 		setVisible(size > 0);
 	});
+
+	useEffect(() => {
+		if (!DisableCoreGui) {
+			return;
+		}
+
+		coreCall("SetCoreGuiEnabled", Enum.CoreGuiType.All, !visible);
+		GuiService.TouchControlsEnabled = !visible;
+	}, [visible, DisableCoreGui]);
 
 	return createPortal(<>{visible ? <blureffect Size={BlurSize} /> : undefined}</>, camera);
 }
