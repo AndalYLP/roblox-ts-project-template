7660bcf76aa99fbdd2e6d8d22189445fb38c3031
AndalYLP <andal089.az@gmail.com>
2025-02-08 13:28:45 -0500

    fix: fixed core-call return/export type

--- src/utils/core-call/index.d.ts
+++ src/utils/core-call/index.d.ts
@@ -3,9 +3,12 @@
  *
  * @param method - The core call method to call.
  * @param args - The arguments to pass to the core.
+ * @returns All return values from the original call.
  * @see https://devforum.roblox.com/t/resetbuttoncallback-has-not-been-registered-by-the-corescripts/78470/8.
  */
-export declare function coreCall<T extends InstanceMethodNames<StarterGui>>(
+declare function coreCall<T extends InstanceMethodNames<StarterGui>>(
 	method: T,
 	...args: Parameters<StarterGui[T]>
-): void;
+): ReturnType<StarterGui[T]>;
+
+export = coreCall;
