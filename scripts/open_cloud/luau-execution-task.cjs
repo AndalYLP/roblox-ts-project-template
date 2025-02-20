90cff21f072e9b9be02093e97c39c2f948e06905
AndalYLP <160054201+AndalYLP@users.noreply.github.com>
2025-02-20 13:51:31 -0500

    fix: stringify data

--- scripts/open_cloud/luau-execution-task.cjs
+++ scripts/open_cloud/luau-execution-task.cjs
@@ -42,7 +42,7 @@ function makeRequest(url, headers, body = null) {
                 throw error;
             }
             attempts++;
-            console.log(`Retrying, error: ${error.message} ${error.response?.data || ''}`);
+            console.log(`Retrying, error: ${error.message} ${JSON.stringify(error.response?.data) || ''}`);
             await new Promise(resolve => setTimeout(resolve, 1000));
             return makeRequestWithRetry();
         }
