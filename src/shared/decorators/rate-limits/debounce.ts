67e2af6834c1f9603029fed83e763f329312dc16
AndalYLP <160054201+AndalYLP@users.noreply.github.com>
2025-02-21 20:57:17 -0500

    feat: added predicate decorator and predicate option for rate-limit decorators

--- src/shared/decorators/rate-limits/debounce.ts
+++ src/shared/decorators/rate-limits/debounce.ts
@@ -1,14 +1,32 @@
 import { debounce } from "@rbxts/set-timeout";
 
-export function Debounce<T extends object, P extends Array<unknown>>(time: number) {
+export function Debounce<T extends object, P extends Array<unknown>>(
+	time: number,
+	predicate?: (...args: P) => boolean,
+) {
 	return (
 		target: T,
 		propertyKey: string,
-		descriptor: TypedPropertyDescriptor<(this: T, ...args: Array<unknown>) => P>,
+		descriptor: TypedPropertyDescriptor<(this: T, ...args: P) => unknown>,
 	) => {
-		const originalMethod = descriptor.value;
+		const debounced = debounce(descriptor.value, time);
 
-		descriptor.value = debounce(originalMethod, time);
+		if (predicate) {
+			let lastResult: unknown;
+			descriptor.value = function (this: T, ...args0: Array<unknown>) {
+				const args = args0 as P;
+
+				if (predicate(...args)) {
+					const result = debounced(this, ...args);
+					lastResult = result;
+					return result;
+				}
+
+				return lastResult;
+			};
+		} else {
+			descriptor.value = debounced;
+		}
 
 		return descriptor;
 	};
