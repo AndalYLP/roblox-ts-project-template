2e05bc595677f87fe71f2f226c688f2816134393
AndalYLP <160054201+AndalYLP@users.noreply.github.com>
2025-03-05 19:30:02 -0500

    feat: add ban command

---
+++ src/server/centurion/commands/moderation.ts
@@ -0,0 +1,56 @@
+import type { CommandContext } from "@rbxts/centurion";
+import { CenturionType, Command, Group, Register } from "@rbxts/centurion";
+import { Players } from "@rbxts/services";
+
+import { username } from "shared/centurion/types/username";
+
+@Register({
+	groups: [
+		{
+			name: "moderation",
+			description: "Moderation related commands",
+		},
+	],
+})
+@Group("moderation")
+export class ModerationCommand {
+	@Command({
+		name: "ban",
+		arguments: [
+			{
+				name: "Username",
+				description: "Player's username to ban from the game.",
+				type: username.name,
+			},
+			{
+				name: "Reason",
+				description: "The reason to ban the player.",
+				type: CenturionType.String,
+			},
+			{
+				name: "Duration",
+				description: "The duration of the ban.",
+				type: CenturionType.Number,
+			},
+		],
+	})
+	public ban(
+		commandContext: CommandContext,
+		userId: number,
+		reason: string,
+		duration: number,
+	): void {
+		try {
+			Players.BanAsync({
+				DisplayReason: reason,
+				Duration: duration,
+				PrivateReason: reason,
+				UserIds: [userId],
+			});
+
+			commandContext.reply(`Banned ${userId}.`);
+		} catch (err) {
+			commandContext.error(`Error banning user ${userId}: ${err}`);
+		}
+	}
+}
